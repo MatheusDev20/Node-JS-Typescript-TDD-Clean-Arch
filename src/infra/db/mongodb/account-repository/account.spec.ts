b37977ad4385b2470940ac92fa0e9730ca8a2757
Matheus <matheusdev20@gmail.com>
2021-11-26 23:01:14 -0300

    test: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -3,7 +3,6 @@ import { Db, MongoClient } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
 describe('Mongodb Account Repository', () => {
-  let connection: MongoClient
   beforeAll(async () => {
     await MongoHelper.connect(global.__MONGO_URI__)
   })
@@ -12,8 +11,15 @@ describe('Mongodb Account Repository', () => {
     await MongoHelper.closeConnection()
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+  const makeSut = (): AccountMongoRepository => {
+    return new AccountMongoRepository()
+  }
   test('Should return an account on insert success', async () => {
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@mail.com',
