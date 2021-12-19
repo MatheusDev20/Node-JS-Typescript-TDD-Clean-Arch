f7110da9ae977816973116b6235c748230221492
Matheus <matheusdev20@gmail.com>
2021-12-18 21:52:24 -0300

    feat: add database connection on server.ts file

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,7 +4,7 @@ export const MongoHelper = {
   client: null as unknown as MongoClient,
   db: null as unknown as Db,
   async connect(uri: string): Promise<void> {
-    this.client = await MongoClient.connect(global.__MONGO_URI__)
+    this.client = await MongoClient.connect(uri)
     this.db = this.client.db()
   },
   async closeConnection(): Promise<void> {
