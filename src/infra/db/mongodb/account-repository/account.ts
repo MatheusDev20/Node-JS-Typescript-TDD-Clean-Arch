92ce90bd549e1915d4fee420cbf4a7bbeb5b8ca2
Matheus <matheusdev20@gmail.com>
2022-02-17 21:11:05 -0300

    feat: ensure error collection creation on mongo

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -8,12 +8,14 @@ export class AccountMongoRepository implements AddAccountRepository {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     const mongoId = result.insertedId
+
     const account = {
       id: String(mongoId),
       name: accountData.name,
       email: accountData.email,
       password: accountData.password
     }
+
     return account
   }
 }
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,8 +3,9 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/useCases/add-acount/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { Controller } from '../../presentation/protocols'
-// import { LogControllerDecorator } from '../decorators/log'
+import { LogControllerDecorator } from '../decorators/log'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
@@ -12,8 +13,7 @@ export const makeSignUpController = (): Controller => {
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAcount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const logMongoRepository = new LogMongoRepository()
   const signUpController = new SignUpController(emailValidator, dbAddAcount)
-  return signUpController
-  // const logErrorRepository = new
-  // return new LogControllerDecorator(signUpController)
+  return new LogControllerDecorator(signUpController, logMongoRepository)
 }
