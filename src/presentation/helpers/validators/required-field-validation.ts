dbef0fe8786d7a39ec2fd0308a2457fbf627c9c5
Matheus <matheusdev20@gmail.com>
2022-03-09 02:17:31 -0300

    refactor: move RequiredFieldValidation to factory method

--- src/presentation/helpers/validators/required-field-validation.ts
+++ src/presentation/helpers/validators/required-field-validation.ts
@@ -1,11 +1,21 @@
 import { MissingParamError } from '../../errors'
 import { RequiredFieldsValidation } from './required-fields-validation'
 
+const makeSut = (): RequiredFieldsValidation => {
+  return new RequiredFieldsValidation('field')
+}
 describe('RequiredFields Validation', () => {
   test('Should return MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldsValidation('field')
+    const sut = makeSut()
     // Não possui a propriedade field
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('Should not return if validation succeed', () => {
+    const sut = makeSut()
+    // Não possui a propriedade field
+    const error = sut.validate({ name: 'any_name', field: 'any_field' })
+    expect(error).toBeFalsy()
   })
 })
