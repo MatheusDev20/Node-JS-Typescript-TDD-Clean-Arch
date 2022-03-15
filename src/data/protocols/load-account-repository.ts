92c8e272c600da65d1577380fdf7daa0cdfe50cf
Matheus <matheusdev20@gmail.com>
2022-03-14 22:59:23 -0300

    feat: adding db-authentication use case for login use case

---
+++ src/data/protocols/load-account-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from '../useCases/add-acount/db-add-account-protocols'
+
+export interface LoadAccountByEmailRepository {
+  load: (email: string) => Promise<AccountModel>
+}
---
+++ src/data/useCases/authentication/db-authentication.ts
@@ -0,0 +1,16 @@
+import { AuthenticationModel, Authentication } from '../../../domain/usecases/authentication'
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-repository'
+
+export class DbAuthentication implements Authentication {
+  private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+
+  constructor(loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository
+  }
+
+  async auth(authentication: AuthenticationModel): Promise<string> {
+    await this.loadAccountByEmailRepository.load(authentication.email)
+
+    return null
+  }
+}
