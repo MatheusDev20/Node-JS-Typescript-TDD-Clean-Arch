36cab76e5412990cd703da7f71ad701e1f4d32f8
Matheus <matheusdev20@gmail.com>
2022-03-18 20:55:16 -0300

    feat: ensure Bcrypt adapter calls bcrypt compare method with correct values

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,33 +1,42 @@
 import bcrypt from 'bcrypt'
-import { Hasher } from '../../data/protocols/cryptograph/hasher'
 import { BcryptAdapter } from './bcrypt-adapter'
 
 jest.mock('bcrypt', () => ({
   async hash(): Promise<string> {
     return await new Promise(resolve => resolve('test_hash'))
+  },
+  async compare(): Promise<boolean> {
+    return await new Promise(resolve => resolve(true))
   }
 }))
 // Mock do metodo hash da lib bcrypt para testar com o hash fixo
 const salt = 12
-const makeSut = (): Hasher => {
+const makeSut = (): BcryptAdapter => {
   return new BcryptAdapter(salt)
 }
 describe('Bcrypt Adapter', () => {
-  test('Should call bcrypt with correct value', async () => {
+  test('Should call hash method with correct value', async () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.hash('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
-  test('Should return a hash on a success', async () => {
+  test('Should return a valid hash on hash method  success', async () => {
     const sut = makeSut()
     const hash = await sut.hash('any_value')
     expect(hash).toBe('test_hash')
   })
-  // test('Should thrown an exception if bcrypt.hash thros', async () => {
-  //   const sut = makeSut()
-  //   jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
-  //   const promise = sut.encrypt('any_value')
-  //   expect(promise).rejects.toThrow()
-  // })
+
+  test('Should call comparer method with correct value', async () => {
+    const sut = makeSut()
+    const compareHashSpy = jest.spyOn(bcrypt, 'compare')
+    await sut.compare('any_value', 'any_hash')
+    expect(compareHashSpy).toHaveBeenCalledWith('any_value', 'any_hash')
+  })
+
+  test('Should return a valid hash on compare method  success', async () => {
+    const sut = makeSut()
+    const hash = await sut.hash('any_value')
+    expect(hash).toBe('test_hash')
+  })
 })
